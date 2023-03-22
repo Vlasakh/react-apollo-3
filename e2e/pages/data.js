43323fecdb97b5209501c6f73c57f9c9b7492b68
vlasakh <zotaper@yandex.ru>
2023-03-22 16:01:44 -0500

    added e2e (WebdriverIO)

---
+++ e2e/pages/data.js
@@ -0,0 +1,24 @@
+const Volodymyr = {
+  firstName: 'Volodymyr',
+  lastName: 'Sakharov',
+  birthDate: '07/05/1984',
+  ssn: '9046',
+  cellPhone: '4699201826',
+  email: 'vvs5102@gmail.com',
+  city: 'Plano',
+  zip: '75093',
+};
+
+const Default = {
+  firstName: 'Fedor',
+  lastName: 'Boyarin',
+  birthDate: '07/05/2000',
+  ssn: '1101',
+  cellPhone: '4699201243',
+  email: 'sendtovova@gmail.com',
+  city: 'Plano',
+  zip: '75093',
+};
+
+// module.exports = Default;
+module.exports = Volodymyr;
